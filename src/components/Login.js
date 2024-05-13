ae95e299e5c5f2ba060eb36da0f4c3760d2b998f
Samson Mubeya <rasspartah@gmail.com>
2024-05-13 23:13:52 +0300

    initial commits

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Login() {
+  return (
+    <>
+    <Navbar/>
+     <div>Login hi hello</div>
+    </>
+   
+  )
+}
+
+export default Login
